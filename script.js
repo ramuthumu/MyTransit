5ff205c42db0a1aeeb9d0c255008a4db1d5f6157
Ramu Thumu <ramuthumu@users.noreply.github.com>
2018-02-08 17:06:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-var requestURL = http://svc.metrotransit.org/NexTrip/535/4/76NE?format=json
+var requestURL = 'http://svc.metrotransit.org/NexTrip/535/4/76NE?format=json'
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
