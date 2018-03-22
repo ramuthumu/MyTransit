66b31d64f66d8392ea7b20cde6846e27ce0eb0aa
Ramu Thumu <ramuthumu@users.noreply.github.com>
2018-03-22 17:04:23 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,6 @@ var links= ["https://svc.metrotransit.org/NexTrip/645/2/SHNA?format=json","https
 for (var i in links) {
     var request = new XMLHttpRequest();
     request.open('GET', links[i],true);
-    request.timeout = 4000;
     
 request.onload = function (i) {
     
