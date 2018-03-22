7d032c70f16b69612596c6db7922d2b087764755
Ramu Thumu <ramuthumu@users.noreply.github.com>
2018-03-22 14:49:09 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,9 +3,10 @@ var links= ["https://svc.metrotransit.org/NexTrip/645/2/SHNA?format=json","https
 
 for (var i in links) {
     var request = new XMLHttpRequest();
-    request.open('GET', links[i], true);
-
-request.onload = function () {
+    request.open('GET', links[i], false);
+    
+request.onload = function (i) {
+    
     // Begin accessing JSON data here
     const app = document.getElementById('root');
     const container = document.createElement('div');
@@ -16,22 +17,31 @@ request.onload = function () {
     var data = JSON.parse(this.response);
     data.forEach(vehicle => {
         // Log each movie's title
+        if(vehicle.Actual==true){
         const card = document.createElement('div');
         card.setAttribute('class', 'card');
         const h1 = document.createElement('h3');
         h1.textContent = vehicle.Description;
         const p = document.createElement('p');
-        p.textContent = vehicle.DepartureText
+        p.textContent = vehicle.DepartureText;
+        const p1 = document.createElement('p');
+        p1.textContent = vehicle.Route;
+        const p2 = document.createElement('p');
+        p2.textContent = vehicle.RouteDirection;
+            
         container.appendChild(card);
         card.appendChild(h1);
         card.appendChild(p);
-
+        card.appendChild(p1);
+        card.appendChild(p2)
+        }
     });
 } else {
-        const errorMessage = document.createElement('marquee');
-        errorMessage.textContent = `Gah, it's not working!`;
+        const errorMessage = document.createElement('h2');
+        errorMessage.textContent = request.status;
         app.appendChild(errorMessage);
 }
 }
+    request.send();
 }
-request.send();
+
